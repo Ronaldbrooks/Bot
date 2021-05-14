7348fa84ed8a64861e5d61a18e80e85a5b5a1997
Ronaldbrooks <Ronominal@gmail.com>
2021-05-13 22:03:55 -0400

    updated

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const client = new Discord.Client();
 const prefix = "$";
 
 client.on("message", function(message) {
-   if (Message.author.bot) return;
+   if (message.author.bot) return;
    if (!message.content.startsWith(prefix)) return;
 
    const commandBody = message.content.slice(prefix.length);
    const args = commandBody.split(' ');
    const command = args.shift().toLowerCase();
 
-   if (command === 'mock') {
-       message.reply()
+   if (command === "mock") {
+       message.reply('fuck you!');
    }
 });
 

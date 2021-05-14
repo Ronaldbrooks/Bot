a55058ad22e3a6a91388924d657074d248c74bac
Ronaldbrooks <Ronominal@gmail.com>
2021-05-13 21:48:02 -0400

    updated

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ const config = require("./config.json");
 
 const client = new Discord.Client();
 
+const prefix = "$";
+
+client.on("message", function(message) {
+   if (Message.author.bot) return;
+   if (!message.content.startsWith(prefix)) return;
+
+   const commandBody = message.content.slice(prefix.length);
+   const args = commandBody.split(' ');
+   const command = args.shift().toLowerCase();
+
+   if (command === 'mock') {
+       message.reply()
+   }
+});
+
 client.login(config.BOT_TOKEN);
